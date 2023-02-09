b58f79d8dade7e9358992b77741085907ae8cccd
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2023-02-09 09:17:13 -0300

    :sparkles: Allow the index in HasIndex to be controlled

--- src/tests/Carousel.test.js
+++ src/tests/Carousel.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount, shallow } from "enzyme";
 import Carousel, { Carousel as CoreCarousel } from "../Carousel";
 import CarouselButton from "../CarouselButton";
 import CarouselSlide from "../CarouselSlide";
@@ -35,6 +35,14 @@ describe("Carousel", () => {
     });
     it("passes `slides` down to the core component", () => {
       expect(wrapper.find(CoreCarousel).prop("slides")).toBe(slides);
+    });
+
+    it("should allow `slideIndex` to be controlled", () => {
+      const mounted = mount(<Carousel slides={slides} slideIndex={1} />);
+      expect(mounted.find(CoreCarousel).prop("slideIndex")).toBe(1);
+
+      mounted.setProps({ slideIndex: 0 });
+      expect(mounted.find(CoreCarousel).prop("slideIndex")).toBe(0);
     });
   });
 
